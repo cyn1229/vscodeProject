a21107d46c1277dccd8214e8e2e0388ef138e910
cyn1229 <cyn_lyy@163.com>
2023-11-25 18:32:36 +0800

    js输入，模板语法

--- 00-练习/project.js
+++ 00-练习/project.js
@@ -1,23 +1,21 @@
-
-var search = function(nums, target) {
-    var start,mid=0,end=nums.length-1;
-    while(start<=end)
-    {
-        mid=((end-start)>>1)+start;
-        if(nums[mid]<target)
-        {
-            start=mid+1;
-        }
-        else if(nums[mid]>target)
-        {
-            end=mid-1;
-        }
-        else
-        {
-            return mid;
-        }
+class ListNode
+{
+    val;
+    next=null;
+    constructor(value){
+        this.val=value;
+        this.next=null;
     }
-    return -1;
-};
-
-console.log(search([-1,0,3,5,9,12],9));
+}
+var removeElements = function(head, val) {
+    const ret = new ListNode(0, head);
+    let cur = ret;
+    while(cur.next) {
+        if(cur.next.val === val) {
+            cur.next =  cur.next.next;
+            continue;
+        }
+        cur = cur.next;
+    }
+    return ret.next;
+}
