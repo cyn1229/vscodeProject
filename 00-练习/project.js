5373269e57d358c26133799e4f691b4cb86b3dc9
cyn1229 <cyn_lyy@163.com>
2023-11-27 13:36:36 +0800

    返回两个值

--- 00-练习/project.js
+++ 00-练习/project.js
@@ -1,21 +1,21 @@
-class ListNode
+let nums=[1,5,8,2,0];
+function getMaxAndMin([])
 {
-    val;
-    next=null;
-    constructor(value){
-        this.val=value;
-        this.next=null;
+    let max=Number.MIN_VALUE;
+    let min=Number.MAX_VALUE;
+    for(i=1;i<nums.length;i++)
+    {
+        // if(max<nums[i])
+        // {
+        //     max=nums[i];
+        // }
+        // if(min>nums[i])
+        // {
+        //     min=nums[i];
+        // }
+        max<nums[i]?max=nums[i]:max;
+        min>nums[i]?min=nums[i]:min;
     }
+    return [max,min]
 }
-var removeElements = function(head, val) {
-    const ret = new ListNode(0, head);
-    let cur = ret;
-    while(cur.next) {
-        if(cur.next.val === val) {
-            cur.next =  cur.next.next;
-            continue;
-        }
-        cur = cur.next;
-    }
-    return ret.next;
-}
+console.log(getMaxAndMin(nums));
