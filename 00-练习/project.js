0b1823eb838053f1b8a2e3e553956e09dc87f504
cyn1229 <cyn_lyy@163.com>
2023-11-29 21:53:00 +0800

    随机抽取名字并显示到页面

--- 00-练习/project.js
+++ 00-练习/project.js
@@ -1,21 +1,5 @@
-let nums=[1,5,8,2,0];
-function getMaxAndMin([])
-{
-    let max=Number.MIN_VALUE;
-    let min=Number.MAX_VALUE;
-    for(i=1;i<nums.length;i++)
-    {
-        // if(max<nums[i])
-        // {
-        //     max=nums[i];
-        // }
-        // if(min>nums[i])
-        // {
-        //     min=nums[i];
-        // }
-        max<nums[i]?max=nums[i]:max;
-        min>nums[i]?min=nums[i]:min;
-    }
-    return [max,min]
+let names=['赵云','黄忠','关羽','张飞','马超','刘备','曹操'];
+function getRandom(min,max){
+    return Math.floor(Math.random()*(max-min+1)-min);
 }
-console.log(getMaxAndMin(nums));
+console.log(names[getRandom(0,names.length-1)]);
